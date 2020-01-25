0ea27523b3380765d3133631e89e70a2ef9c6490
Sascha Andeweg <sascha@designed.irish>
2020-01-25 17:21:44 +0000

    commit of the method chaining challenge 1

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
  
-
-
-
+    $('#button1').mouseenter(function(){
+        $('#button1').removeClass('makeRed').addClass('makeBorder');
+    })
+    $('#button1').mouseleave(function(){
+        $('#button1').removeClass('makeBorder').addClass('makeRed');
+    })
 }); 
