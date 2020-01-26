8da8ea7614fa7f03f51fca62390bd68c2862de56
Sascha Andeweg <sascha@designed.irish>
2020-01-26 10:06:00 +0000

    commit of last bit of the video of unit this

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
-$(document).ready(function(){
-    $(".box").on("click", function(){
+$(document).ready(function() {
+    $(".box").on("click", function() {
+            // when clicking element with 'box' class:
         var classNames = $(this).attr("class").split(" ");
-        $("." + classNames[1]).css("background-color", "red");
+        var boxClass = classNames[0];
+        var className = classNames[1];
+        if ($(this).css("background-color") == "rgb(255, 0, 0)") {
+            $("." + className).css("background-color", "#000");
+        } else {
+            // else turn all box class elements black en then change all 
+            // similar elements as clicked to red
+            $("." + boxClass).css("background-color", "#000");
+            $("." + className).css("background-color", "red");
+        }
     });
 });
